a6a54e507bfc086249b7654943ac12535a990d70
Yuki <youxiyuannei5@gmail.com>
2021-01-17 19:34:00 +0900

    issue_7: 飲食店の情報を取得するメソッド

--- src/edar/src/App.tsx
+++ src/edar/src/App.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import base64 from 'base-64';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ class RestaurantList extends Component {
     latitude: null,
     longitude: null,
     isLoaded: false,
+    isLoadedShopInfo: false,
+    shops: [],
     url: "",
   };
 
@@ -30,12 +34,28 @@ class RestaurantList extends Component {
     );
   }
 
+  // 飲食店の情報を取得
+  getShopInfo() {
+    axios.get(this.state.url, {
+      headers: {
+        'Authorization': 'Basic ' + base64.encode(process.env['REACT_APP_USER'] + ":" + process.env['REACT_APP_PASSWORD']),
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((res) => {
+        this.setState({ shops: res.data.results.shop, isLoadedShopInfo: true });
+      })
+      .catch((err) => {
+        this.setState({ isLoadedShopInfo: false });
+      })
+      );
   }
 
   // 条件よりURLを設定
   setLocationInfoToURL() {
     let url: string = "https://quiet-hamlet-14379.herokuapp.com/hgs?lat=" + this.state.latitude + "&lng=" + this.state.longitude + "&range=4&order=1";
     this.setState({ url: url });
+    this.getShopInfo();
   }
 
 
