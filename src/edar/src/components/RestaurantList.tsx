c141338133762f37763d1d1e17d04d05ed2d3f2e
Yuki <youxiyuannei5@gmail.com>
2021-01-27 00:06:52 +0900

    issue_16: RestaurantListクラスをRestaurantList関数に変更

--- src/edar/src/components/RestaurantList.tsx
+++ src/edar/src/components/RestaurantList.tsx
@@ -1,97 +1,97 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import base64 from 'base-64';
 
-class RestaurantList extends Component {
-    state = {
-        latitude: null,
-        longitude: null,
-        isLoaded: false,
-        isLoadedShopInfo: false,
-        isPushed: false,
-        shops: [],
-        url: "",
-        errorMessage: "",
-    };
-
-    // 初期化
-    constructor(props: any) {
-        super(props);
-        this.getLocationInfo = this.getLocationInfo.bind(this);
-    }
+const RestaurantList = () => {
+    let [latitude, setLatitude] = useState(0);
+    let [longitude, setLongitude] = useState(0);
+    let [isLoadedLocationInfo, setIsLoadedLocationInfo] = useState(false);
+    let [isLoadedShopInfo, setIsLoadedShopInfo] = useState(false);
+    let [isPushed, setIsPushed] = useState(false);
+    let [shops, setShops] = useState([]);
+    let [url, setUrl] = useState("");
+    let [errorMessage, setErrorMessage] = useState("");
 
     // 経度緯度情報を取得
-    getLocationInfo() {
+    const getLocationInfo = () => {
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                this.setState({ longitude: position.coords.longitude, latitude: position.coords.latitude, isLoaded: true });
-                this.setLocationInfoToURL();
+                setIsLoadedLocationInfo(true);
+                setLongitude(position.coords.longitude);
+                setLatitude(position.coords.latitude);
             },
             (error) => {
-                let errMsg;
                 switch (error.code) {
                     case 1:
-                        errMsg = "位置情報の利用が許可されていません";
+                        errorMessage = "位置情報の利用が許可されていません";
                         break;
                     case 2:
-                        errMsg = "デバイスの位置が判定できません";
+                        errorMessage = "デバイスの位置が判定できません";
                         break;
                     case 3:
-                        errMsg = "タイムアウトしました";
+                        errorMessage = "タイムアウトしました";
                         break;
                 }
-                this.setState({ isPushed: true, isLoaded: false, errorMessage: errMsg });
+                setIsPushed(true);
+                setIsLoadedLocationInfo(false);
+                setErrorMessage(errorMessage);
             }
         );
-    }
+    };
+
+    const setLocationInfoToURL = () => {
+        url = process.env['REACT_APP_RSTRNT_API_URL'] + '/hgs?lat=' + latitude + '&lng=' + longitude + '&range=4&order=1&genre=G006';
+        setUrl(url);
+        getShopInfo();
+    };
 
     // 飲食店の情報を取得
-    getShopInfo() {
-        axios.get(this.state.url, {
+    const getShopInfo = () => {
+        axios.get(url, {
             headers: {
                 'Authorization': 'Basic ' + base64.encode(process.env['REACT_APP_RSTRNT_API_USER'] + ":" + process.env['REACT_APP_RSTRNT_API_PASSWORD']),
                 'Content-Type': 'application/json'
             }
         })
             .then((res) => {
-                this.setState({ shops: res.data.results.shop, isLoadedShopInfo: true });
+                setShops(res.data.results.shop);
+                setIsLoadedShopInfo(true);
             })
             .catch((err) => {
-                this.setState({ isLoadedShopInfo: false });
+                setIsLoadedShopInfo(false);
             })
             .finally(() => {
-                this.setState({ isPushed: true });
+                setIsPushed(true);
             }
             );
-    }
+    };
 
-    // 条件よりURLを設定
-    setLocationInfoToURL() {
-        let url: string = process.env['REACT_APP_RSTRNT_API_URL'] + '/hgs?lat=' + this.state.latitude + '&lng=' + this.state.longitude + '&range=4&order=1';
-        this.setState({ url: url });
-        this.getShopInfo();
-    }
+    // 経度、緯度が更新されたらsetLocationInfoToURL関数を呼び出す。
+    useEffect(() => {
+        if (isLoadedLocationInfo) {
+            setLocationInfoToURL();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [longitude, latitude]);
 
-    render() {
-        return (
-            <div className="shopList">
-                <button onClick={this.getLocationInfo}>
-                    現在地よりお店を検索
-                </button>
-                <ol>
-                    {this.state.shops.map((index: any) => (
-                        <li key={index.id}>{index.name}</li>
-                    ))}
-                    {this.state.isPushed &&
-                        !this.state.isLoaded &&
-                        <p> {this.state.errorMessage}</p>}
-                    {(this.state.isPushed && this.state.isLoaded) &&
-                        !this.state.isLoadedShopInfo &&
-                        <p>お店の情報を取得できませんでした。</p>}
-                </ol>
-            </div >
-        )
-    }
+    return (
+        <div className="shopList">
+            <button onClick={getLocationInfo}>
+                現在地よりお店を検索
+            </button>
+            <ol>
+                {shops.map((index: any) => (
+                    <li key={index.id}>{index.name}</li>
+                ))}
+                {isPushed &&
+                    !isLoadedLocationInfo &&
+                    <p> {errorMessage}</p>}
+                {(isPushed && isLoadedLocationInfo) &&
+                    !isLoadedShopInfo &&
+                    <p>お店の情報を取得できませんでした。</p>}
+            </ol>
+        </div>
+    )
 }
 
 export default RestaurantList;
