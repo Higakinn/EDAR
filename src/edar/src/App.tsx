e15ad179386113bb6f16d86327b9889fb75475cd
yuuki higaki <yuukihigaki@yuukinoMacBook-Pro.local>
2021-01-30 13:27:37 +0900

    issue #21 : 作成したコンポーネントをApp.tsxから呼び出し

--- src/edar/src/App.tsx
+++ src/edar/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import RestaurantList from './components/RestaurantList';
-
+import Header from './components/Header'
+import EdarSiteContent from './components/EdarSiteContent'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
+      <Header title="EDAR" subtitle="~ Easily decide on a restaurant ~" />
+      <EdarSiteContent />
       <RestaurantList />
+      <Footer title="EDAR" description="~ Easily decide on a restaurant ~" />
     </div>
   );
 }
