34dc7c46e5af2d77d6be9f8501a4807296438a12
yuuki higaki <yuukihigaki@yuukinoMacBook-Pro.local>
2021-01-30 15:42:38 +0900

    issue #21 : 画面上部に表示される画像を食品に関連した画像をランダムに表示するように修正

--- src/edar/src/components/EdarSiteContent.tsx
+++ src/edar/src/components/EdarSiteContent.tsx
@@ -20,14 +20,17 @@ const useStyles = makeStyles({
 
 export default function SiteContents() {
     const classes = useStyles();
+    //https://picsum.photos/images より食品にかんする画像IDを抽出
+    const foodImgIds: number[] = [292, 488, 493, 674, 75, 766, 824, 1060, 1080, 225]
+    const foodImgId: number = foodImgIds[Math.floor(Math.random() * foodImgIds.length)]
     return (
         <Grid container>
             <Grid item xs={12} sm={12}>
                 <Card className={classes.root}>
                     <CardMedia
                         className={classes.media}
-                        image="https://picsum.photos/400/400"
-                        title="sample"
+                        image={`https://picsum.photos/id/${foodImgId}/400/400`}
+                        title="food image"
                     />
                 </Card>
             </Grid>
