5e82934bae14c1949b7cd3ee78eec6dfa4a79bb3
yuuki higaki <yuukihigaki@yuukinoMacBook-Pro.local>
2021-02-07 00:43:50 +0900

    issue#21 : テストファイルをコンポーネント毎に切り分け

---
+++ src/edar/src/components/Header.test.tsx
@@ -0,0 +1,10 @@
+import Footer from './Footer';
+import { render, screen } from '@testing-library/react';
+
+test('header: 指定した引数がレンダリングされているかどうか', () => {
+    render(<Header title="Test" subtitle="testtest" />);
+    const titleText = screen.getByText(/Test/);
+    const subtitleText = screen.getByText(/testtest/);
+    expect(titleText).toBeInTheDocument();
+    expect(subtitleText).toBeInTheDocument();
+});
