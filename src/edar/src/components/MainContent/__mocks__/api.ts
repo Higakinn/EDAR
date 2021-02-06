5acb2b8a3692c175c02e13dfc2090b63d3e7c8f3
Yuki <youxiyuannei5@gmail.com>
2021-02-07 02:08:04 +0900

    issue_16: コメントアウトの削除

--- src/edar/src/components/MainContent/__mocks__/api.ts
+++ src/edar/src/components/MainContent/__mocks__/api.ts
@@ -1,82 +1,3 @@
-// __mocks__/request.js
-// const genreList = {
-//     "results": {
-//         "api_version": "1.20",
-//         "genre": [
-//             {
-//                 "code": "G001",
-//                 "name": "居酒屋"
-//             },
-//             {
-//                 "code": "G002",
-//                 "name": "ダイニングバー・バル"
-//             },
-//             {
-//                 "code": "G003",
-//                 "name": "創作料理"
-//             },
-//             {
-//                 "code": "G004",
-//                 "name": "和食"
-//             },
-//             {
-//                 "code": "G005",
-//                 "name": "洋食"
-//             },
-//             {
-//                 "code": "G006",
-//                 "name": "イタリアン・フレンチ"
-//             },
-//             {
-//                 "code": "G007",
-//                 "name": "中華"
-//             },
-//             {
-//                 "code": "G008",
-//                 "name": "焼肉・ホルモン"
-//             },
-//             {
-//                 "code": "G017",
-//                 "name": "韓国料理"
-//             },
-//             {
-//                 "code": "G009",
-//                 "name": "アジア・エスニック料理"
-//             },
-//             {
-//                 "code": "G010",
-//                 "name": "各国料理"
-//             },
-//             {
-//                 "code": "G011",
-//                 "name": "カラオケ・パーティ"
-//             },
-//             {
-//                 "code": "G012",
-//                 "name": "バー・カクテル"
-//             },
-//             {
-//                 "code": "G013",
-//                 "name": "ラーメン"
-//             },
-//             {
-//                 "code": "G016",
-//                 "name": "お好み焼き・もんじゃ"
-//             },
-//             {
-//                 "code": "G014",
-//                 "name": "カフェ・スイーツ"
-//             },
-//             {
-//                 "code": "G015",
-//                 "name": "その他グルメ"
-//             }
-//         ],
-//         "results_available": 17,
-//         "results_returned": "17",
-//         "results_start": 1
-//     }
-// };
 import type { Genre, Shop } from '../../MainContent/MainContent';
 
 export async function getGenre(): Promise<Genre[]> {
@@ -149,12 +70,9 @@ export async function getGenre(): Promise<Genre[]> {
             "code": "G015",
             "name": "その他グルメ"
         }
-    ]
+    ];
     return new Promise((resolve, reject) => {
         resolve(genreList);
-        // process.nextTick(() => {
-        //     resolve(genreList);
-        // })
     })
 }
 
@@ -343,14 +261,13 @@ export async function getShopList(url: string): Promise<Shop[]> {
             "wifi": "あり"
         }
     ];
-
     return new Promise((resolve, reject) => {
         resolve(shops);
     });
 }
 
 export async function getPosition(): Promise<{ latitude: number, longitude: number }> {
-    const position = { latitude: 34.8225536, longitude: 135.4268672 }
+    const position = { latitude: 34.8225536, longitude: 135.4268672 };
     return new Promise((resolve, reject) => {
         process.nextTick(() =>
             resolve(position)
