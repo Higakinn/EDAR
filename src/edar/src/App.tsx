203bb7bc7f07a9037d16cc5832ec56b835fd8e89
yuuki higaki <yuukihigaki@yuukinoMacBook-Pro.local>
2021-02-07 11:44:44 +0900

    issue#21 : コンポーネント名の修正(EdarSiteContents→EdarSiteTop)

--- src/edar/src/App.tsx
+++ src/edar/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
 import RestaurantList from './components/RestaurantList';
 import Header from './components/Header'
-import EdarSiteContent from './components/EdarSiteContent'
+import EdarSiteTop from './components/EdarSiteTop'
 import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
       <Header title="EDAR" subtitle="~ Easily decide on a restaurant ~" />
-      <EdarSiteContent />
+      <EdarSiteTop />
       <RestaurantList />
       <Footer title="EDAR" description="~ Easily decide on a restaurant ~" />
     </div>
--- src/edar/src/components/EdarSiteContent.test.tsx
+++
@@ -1,7 +0,0 @@
-import EdarSiteContent from './EdarSiteContent';
-import { render, screen } from '@testing-library/react';
-
-test('EdarSiteContent: 適切なtitleが取得できるかどうか', () => {
-    const container = render(<EdarSiteContent />);
-    expect(container.getByTitle('food image')).toBeInTheDocument();
-});
