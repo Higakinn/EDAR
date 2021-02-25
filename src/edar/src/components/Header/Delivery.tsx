ee20a7206021d5f23bba5f7fae9a390b345020fb
Yuki <youxiyuannei5@gmail.com>
2021-02-26 00:38:11 +0900

    issue #32 : 各ボタン(ログインボタンなど）のコンポーネントをファイル別に作成。

---
+++ src/edar/src/components/Header/Delivery.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
+export default function Delivery(props: { message: string, icon: React.ElementType<any> }) {
+    const classes = useStyles();
+    const { message, icon } = props;
+
+    return (
+        <>
+            <IconButton aria-label="login-button">
+                {React.createElement(icon, { color: 'primary' })}
+                <Typography variant="caption" className={classes.sectionMobile}>{message}</Typography>
+            </IconButton>
+        </>
+    );
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        sectionMobile: {
+            display: 'flex',
+            marginLeft: theme.spacing(1),
+            [theme.breakpoints.down('sm')]: {
+                display: 'none',
+            }
+        }
+    }),
+);
