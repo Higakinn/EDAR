dea3a621fed5d053c32f0f5e3ac5704237e4b375
Yuki <youxiyuannei5@gmail.com>
2021-02-27 08:13:20 +0900

    issue #32 : ユーザー情報をreduxで管理

--- src/edar/src/stores/rootReducer.ts
+++ src/edar/src/stores/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import shopInformationReducer from './shopInformation';
+import userInfomationReducer from './userInfomation';
 
 const rootReducer = combineReducers({
     shopInformation: shopInformationReducer,
+    userInfomation: userInfomationReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ src/edar/src/stores/userInfomation.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type State = {
+    isLogining: boolean
+    user: {
+        email: string | null
+        displayName: string | null
+    }
+}
+
+const initialState: State = {
+    isLogining: false,
+    user: {
+        email: null,
+        displayName: null,
+    }
+};
+
+const slice = createSlice({
+    name: 'userInfomation',
+    initialState,
+    reducers: {
+        updateUserInfomation: (state: State, action: PayloadAction<{ email: string | null, displayName: string | null }>) => {
+            state.user = action.payload;
+            state.isLogining = true;
+        },
+        clearUserInfomation: (state: State) => {
+            state.user = { email: null, displayName: null };
+            state.isLogining = false;
+        }
+    }
+});
+
+// action creatorをエクスポート
+export const {
+    updateUserInfomation,
+    clearUserInfomation,
+} = slice.actions;
+
+// reducerをエクスポート
+export default slice.reducer;
