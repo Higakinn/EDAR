9ad50f5ca6a7378861454267b102a5372ea43db2
Yuki <youxiyuannei5@gmail.com>
2021-02-27 08:42:23 +0900

    issue #32 : テストコードの修正

--- src/edar/src/components/Header/Header.test.tsx
+++ src/edar/src/components/Header/Header.test.tsx
@@ -1,8 +1,14 @@
 import Header from './Header';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux'
+import store from '../../store';
 
 test('header: 指定した引数がレンダリングされているかどうか', () => {
-    render(<Header title="Test" subtitle="testtest" />);
+    render(
+        <Provider store={store}>
+            <Header title="Test" subtitle="testtest" />
+        </Provider>
+    );
     const titleText = screen.getByText(/Test/);
     const subtitleText = screen.getByText(/testtest/);
     expect(titleText).toBeInTheDocument();
