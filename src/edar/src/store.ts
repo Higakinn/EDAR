43b3d883dcfe2d4a6fb53a1b48354ae1d162afcd
yuki <51454617+Yuki-TU@users.noreply.github.com>
2021-04-01 21:44:43 +0900

    issue #36 : reduxのactionでは非同期処理を扱うため、それを可能とさせるredux-thunkを導入

--- src/edar/src/store.ts
+++ src/edar/src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { persistedReducer } from './stores/rootReducer';
 import { persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: [thunk]
 });
 
 export const persistor = persistStore(store);
